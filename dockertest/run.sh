aa7d69a1054fdfd97c09980d91180e047836b744
kewool <kkttyy324@gmail.com>
2021-11-14 18:00:06 +0900

    docker

---
+++ dockertest/run.sh
@@ -0,0 +1,2 @@
+uvicorn main:app --host=0.0.0.0 --port=3000
+sleep 300
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
@@ -6,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get('/ctf')
+def root():
+    token="abcd"
+    os.system(f"docker run --name {token} -p 3000 ctf_1")
+    return 'ctf'
