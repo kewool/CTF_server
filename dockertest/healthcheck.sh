2d2c2058f2c95db04a2ef6314619399da7583209
kewool <kkttyy324@gmail.com>
2021-11-17 16:03:30 +0900

    docker

--- dockertest/healthcheck.sh
+++ dockertest/healthcheck.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 NET=netstat -tupn | grep 3000
+#TOKEN_VAL=`printenv TOKEN`
+TOKEN='abcd'
 
-echo ${NET}
+if [ $NET = '']
+then
+    curl `https://ctf.kewool.net/api/ctf/${TOKEN}`
+fi
